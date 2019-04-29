dfd327eb80109256b2195d9bc486f7678a46e20b
srijitcoder <srijitsm123@gmail.com>
2019-04-30 02:32:05 +0530

    React Structural Code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react"
+import validator from 'validator'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoading:false,
+            searchData: '',
+            photos: [],
+            next: 1
+        }
+        window.onscroll = () => {
+            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+                this.getPhotos();
+            }
+        };
+
+    }
+
+    componentDidMount() {
+        this.getPhotos()
+    }
+
+    handleChange(e) {
+        this.setState({
+            searchData: e.target.value
+        });
+    }
+
+    keyPress(e) {
+        if(e.keyCode === 13) {
+            this.setState({
+                isLoading:false,
+                searchData: e.target.value,
+                photos: [],
+                next: 1
+            }, function(){
+                this.getPhotos()
+            })         
+        }
+    }
+
+    getPhotos() {
+        console.log(this.state)
+        const API_KEY = "3a132b2bb78af8acd1d62ca9b0d7389f"
+        let METHOD = "flickr.photos.search"
+
+        if (validator.isEmpty(this.state.searchData)) 
+            METHOD = "flickr.photos.getRecent"
+
+
+        if(this.state.next !== null) {
+            fetch(`https://api.flickr.com/services/rest/?page=${this.state.next}&method=${METHOD}&api_key=${API_KEY}&text=${this.state.searchData}&format=json&nojsoncallback=1`, {
+                method: 'GET'
+            })
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                if(data.photos.photo.length > 0) {
+                    let next = data.photos.page+1
+                    if(data.photos.page === data.photos.pages) {
+                        next = null 
+                    }
+
+                    const photos = data.photos.photo.map(photo => ({
+                        id: photo.id,
+                        owner: photo.owner,
+                        secret: photo.secret,
+                        server: photo.server,
+                        farm: photo.farm,
+                        title: photo.title,
+                        url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`
+                    }));
+                    
+                    this.setState({
+                        next: next,
+                        photos: [
+                            ...this.state.photos,
+                            ...photos
+                        ]
+                    })
+                }
+            })
+        }
+    }
+
+    render(){
+        const {
+            searchData,
+            isLoading,
+            photos,
+            next,
+        } = this.state;
+
+        return(
+            <>
+                <input type="search" value={searchData} onKeyDown={this.keyPress.bind(this)} onChange={this.handleChange.bind(this)} placeholder="Search" />
+                {photos.map(photo => (
+                    <Fragment key={photo.id}>
+                        <img src={photo.url} />
+                    </Fragment>
+                ))}
+            </>
+        )
+    }
 }
 
 export default App;
